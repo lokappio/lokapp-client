82bd451ebcb721691fd78b7fc31def388374dd1b
Clémence Roumy <clemence@playmoweb.com>
2022-01-28 09:57:29 +0100

    fix css dashboard

--- src/components/molecules/dashboard/MyProjects.vue
+++ src/components/molecules/dashboard/MyProjects.vue
@@ -1,17 +1,19 @@
 <template>
-  <v-container fluid class="my-projects-container pa-5">
+  <v-container fluid class="my-projects-container pa-0">
     <!-- Header -->
-    <h2 class="title-h2">{{ this.$t("dashboard.all_projects_title") }}</h2>
-    <p class="text-2 grey-color d-block">{{ this.$tc("dashboard.last_update", timerUpdate, {count: timerUpdate}) }}</p>
+    <div class="header ml-5">
+      <h2 class="title-h2">{{ this.$t("dashboard.all_projects_title") }}</h2>
+      <p class="text-2 grey-color d-block">{{ this.$tc("dashboard.last_update", timerUpdate, {count: timerUpdate}) }}</p>
+    </div>
 
     <!-- Projects -->
-    <v-row no-gutters class="mt-3">
+    <v-row no-gutters class="mt-3 content">
       <v-col v-if="loading" cols="12" class="text-center">
         <v-progress-circular color="primary" indeterminate></v-progress-circular>
       </v-col>
 
-      <v-col v-else cols="12">
-        <v-row class="projects-container-style ma-0 mr-5" v-if="displayedProjects.length">
+      <v-col v-else cols="12" class="projects-container-style">
+        <v-row class="ma-0 mr-3" v-if="displayedProjects.length">
           <v-col
               v-for="project in displayedProjects"
               :key="project.id"
@@ -94,13 +96,21 @@ export default Vue.extend({
 .my-projects-container {
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
-  height: 100%;
   background-color: var(--v-background-base);
+}
+
+.header {
+  margin-top: 30px;
+  height: 50px;
+}
+.content {
+  position: absolute;
+  top: 80px;
+  bottom: 0;
 }
 
 .projects-container-style {
   height: 100%;
   overflow-y: scroll;
-  overflow-x: hidden;
 }
 </style>
