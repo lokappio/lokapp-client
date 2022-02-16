1a5594ee0218ba9d5bf40d064d2f7fd2f8e98f25
Clémence Roumy <clemence@playmoweb.com>
2022-02-16 11:42:55 +0100

    fix close popup

--- src/components/molecules/buttons/InvitationsButton.vue
+++ src/components/molecules/buttons/InvitationsButton.vue
@@ -1,49 +1,51 @@
 <template>
-  <v-dialog v-model="dialogOpened" scrollable transition="dialog-bottom-transition" max-width="600px">
-    <template v-slot:activator="{ on }">
-      <v-badge :value="nbOfInvitations > 0" color="red" dot overlap>
-        <v-btn v-on="on" x-small fab depressed color="primary">
-          <v-icon color="white">mdi-bell-ring</v-icon>
-        </v-btn>
-      </v-badge>
-    </template>
+  <div>
+    <v-dialog v-model="dialogOpened" scrollable transition="dialog-bottom-transition" max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-badge :value="nbOfInvitations > 0" color="red" dot overlap>
+          <v-btn v-on="on" x-small fab depressed color="primary">
+            <v-icon color="white">mdi-bell-ring</v-icon>
+          </v-btn>
+        </v-badge>
+      </template>
 
-    <v-card>
-      <v-card-title>
-        {{ $t("invitations.title") }}
-        <v-spacer></v-spacer>
-        <v-icon @click="opened = !opened" color="black">mdi-close</v-icon>
-      </v-card-title>
+      <v-card>
+        <v-card-title>
+          {{ $t("invitations.title") }}
+          <v-spacer></v-spacer>
+          <v-icon @click="dialogOpened = false" color="black">mdi-close</v-icon>
+        </v-card-title>
 
-      <v-card-text class="list-invitations-style">
-        <v-list>
-          <v-list-item class="px-0" v-for="invitation in invitations" :key="invitation.id">
-            <v-list-item-content>
-              <v-list-item-title v-if="invitation.ownerUsername" v-text="$t('invitations.item_owner', { owner: invitation.ownerUsername, project: invitation.projectName })"></v-list-item-title>
-              <v-list-item-title v-else v-text="$t('invitations.item_no_owner', { project: invitation.projectName })"></v-list-item-title>
-            </v-list-item-content>
+        <v-card-text class="list-invitations-style">
+          <v-list>
+            <v-list-item class="px-0" v-for="invitation in invitations" :key="invitation.id">
+              <v-list-item-content>
+                <v-list-item-title v-if="invitation.ownerUsername" v-text="$t('invitations.item_owner', { owner: invitation.ownerUsername, project: invitation.projectName })"></v-list-item-title>
+                <v-list-item-title v-else v-text="$t('invitations.item_no_owner', { project: invitation.projectName })"></v-list-item-title>
+              </v-list-item-content>
 
-            <v-list-item-action>
-              <v-container class="justify-center">
-                <v-row>
-                  <v-col class="pa-0 pr-1" cols="6">
-                    <v-btn @click="acceptInvitation(invitation)" depressed color="primary">
-                      <v-icon>mdi-check</v-icon>
-                    </v-btn>
-                  </v-col>
-                  <v-col class="pa-0 pl-1" cols="6">
-                    <v-btn @click="declineInvitation(invitation)" depressed outlined color="primary">
-                      <v-icon>mdi-close</v-icon>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-list-item-action>
-          </v-list-item>
-        </v-list>
-      </v-card-text>
-    </v-card>
-  </v-dialog>
+              <v-list-item-action>
+                <v-container class="justify-center">
+                  <v-row>
+                    <v-col class="pa-0 pr-1" cols="6">
+                      <v-btn @click="acceptInvitation(invitation)" depressed color="primary">
+                        <v-icon>mdi-check</v-icon>
+                      </v-btn>
+                    </v-col>
+                    <v-col class="pa-0 pl-1" cols="6">
+                      <v-btn @click="declineInvitation(invitation)" depressed outlined color="primary">
+                        <v-icon>mdi-close</v-icon>
+                      </v-btn>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script lang="ts">
@@ -57,7 +59,6 @@ export default Vue.extend({
   },
   data() {
     return {
-      opened: false,
       timer: null,
       invitations: [],
       dialogOpened: false
