0032e00ffeaf961421ec4d25cb6759640732f51b
Clémence Roumy <clemence@playmoweb.com>
2022-02-17 10:54:43 +0100

    isLoggedIn if user infos from api is retrieved

--- src/data/services/AuthService.ts
+++ src/data/services/AuthService.ts
@@ -65,7 +65,7 @@ class AuthService {
   }
 
   public static isLoggedIn(): boolean {
-    return FirebaseHelper.isLoggedIn();
+    return FirebaseHelper.isLoggedIn() && store.getters.appUser != null;
   }
 }
 
