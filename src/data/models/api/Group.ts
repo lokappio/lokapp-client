e416a724aee08b2f0de6aca404eb56d3c835e484
Clémence Roumy <clemence@playmoweb.com>
2022-02-18 11:46:33 +0100

    revert modif common group

--- src/data/models/api/Group.ts
+++ src/data/models/api/Group.ts
@@ -5,7 +5,7 @@ export default class Group {
   name: string;
   keys: Key[] = [];
 
-  get isDefault(): boolean { return this.id === 1}
+  get isDefault(): boolean { return this.name === "common"}
   get isNewGroup(): boolean { return this.id === -1}
 
   public static map(data: Partial<Group>): Group {
