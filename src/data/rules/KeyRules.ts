1774ea408da3c37c0948d9416f61c890976b2743
Clémence Roumy <clemence@playmoweb.com>
2022-05-11 11:56:06 +0200

    remove snake_case mandatory on key and group name

--- src/data/rules/KeyRules.ts
+++ src/data/rules/KeyRules.ts
@@ -3,5 +3,4 @@ import i18n from "@/i18n";
 export const keyNameRules = () => [
     (v: string) => !!v || i18n.tc('rules.required'),
     (v: string) => v.length > 0 || i18n.tc("rules.key_name_length"),
-    (v: string) => /^([a-z0-9]*)(([a-z0-9]|_)*)([a-z0-9]+)$/.test(v) || i18n.tc("rules.snake_case_only")
 ];
