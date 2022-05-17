7dfdbb55e04c31b138604762adbf4957af492b19
Clémence Roumy <clemence@playmoweb.com>
2022-05-17 14:18:26 +0200

    import from json : manage case of keys with no groups

--- src/data/services/ProjectsService.ts
+++ src/data/services/ProjectsService.ts
@@ -34,7 +34,7 @@ class ProjectsService {
     const projectImport = await ImportService.importFromFiles(project, items);
     console.log(projectImport);
 
-    /*const createdProject = await this.createProject(project, items.map((item) => item.language));
+    const createdProject = await this.createProject(project, items.map((item) => item.language));
     createdProject.languages = await LanguagesService.getLanguages(createdProject.id);
     createdProject.groups = await GroupsService.getGroups(createdProject.id);
 
@@ -58,7 +58,7 @@ class ProjectsService {
     }));
 
     localStorage.setItem(createdProject.id.toString(), JSON.stringify(projectImport.warnings));
-    return createdProject.id;*/
+    return createdProject.id;
   }
 
   public static async getEntireProjectById(projectId: number): Promise<Project> {
