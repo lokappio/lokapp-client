80120bd5a9ef11f1e63578cd5bbc063185bb0595
Clémence Roumy <clemence@playmoweb.com>
2022-06-30 15:48:03 +0200

    test all created project

--- src/data/models/ImportItem.ts
+++ src/data/models/ImportItem.ts
@@ -1,9 +1,21 @@
+export class ItemIOS {
+  extension: string;
+  content: string
+
+  constructor(extension: string, content: string) {
+    this.extension = extension;
+    this.content = content;
+  }
+}
+
 export default class ImportItem {
   language: string;
-  content: File[] | File | string;
+  content: File[] | File | string | ItemIOS[];
+  fromTest: boolean;
 
-  constructor(language: string, content: File[] | File | string) {
+  constructor(language: string, content: File[] | File | string | ItemIOS[], fromTest = false) {
     this.language = language;
     this.content = content;
+    this.fromTest = fromTest;
   }
 }
